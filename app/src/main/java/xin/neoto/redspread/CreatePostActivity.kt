a55c75ea089f375883a1c0fddde0bd7a2bebcaaf
Kyle Zheng <kylezheng73@gmail.com>
2019-10-13 02:14:07 -0400

    forgive father for i have sinned

---
+++ app/src/main/java/xin/neoto/redspread/CreatePostActivity.kt
@@ -0,0 +1,12 @@
+package xin.neoto.redspread.dummy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreatePostActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_post)
+    }
+}
